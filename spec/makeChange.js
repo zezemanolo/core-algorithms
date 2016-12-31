7fbc466634f42a634dbbd387217e5a1f89664999
Tanner Welsh <tannerwelsh@gmail.com>
2016-12-31 11:28:17 -0500

    Use babel + ES2015

--- spec/makeChange.js
+++ spec/makeChange.js
@@ -1,50 +1,44 @@
-const expect = require('chai').expect
+import { expect } from 'chai'
+import makeChange from '../src/makeChange'
 
-const makeChange = require('../src/makeChange')
-
-describe('makeChange()', function() {
+describe('makeChange()', () => {
   'use strict'
 
-  it('exists', function() {
+  it('exists', () => {
     expect(makeChange).to.be.a('function')
   })
 
-  it('returns an object with all coin types (quarters, dimes, nickels, and pennies)', function() {
+  it('returns an object with all coin types (quarters, dimes, nickels, and pennies)', () => {
     const price = 1.00
     const amountGiven = 1.00
 
-    expect(makeChange(price, amountGiven)).to.have.keys('quarters', 'dimes', 'nickels', 'pennies')
+    expect(makeChange({price, amountGiven})).to.have.keys('quarters', 'dimes', 'nickels', 'pennies')
   })
 
-  it('returns correct change', function() {
+  it('returns correct change', () => {
     const price = 1.00
     const amountGiven = 1.00 + 0.25 + 0.10 + 0.05 + 0.01
     const expectedChange = { quarters: 1, dimes: 1, nickels: 1, pennies: 1 }
 
-    expect(makeChange(price, amountGiven)).to.deep.equal(expectedChange)
+    expect(makeChange({price, amountGiven})).to.deep.equal(expectedChange)
   })
 
-  it('minimizes the number of coins given by using more high-value coins', function() {
+  it('minimizes the number of coins given by using more high-value coins', () => {
     const price = 1.00
 
-    const noQuarters = 2
-    const noDimes    = 1
-    const noNickels  = 1
-    const noPennies  = 3
+    const quarters = 2
+    const dimes    = 1
+    const nickels  = 1
+    const pennies  = 3
 
     const amountGiven = 1.00
-                      + (noQuarters * 0.25)
-                      + (noDimes * 0.10)
-                      + (noNickels * 0.05)
-                      + (noPennies * 0.01)
+                      + (quarters * 0.25)
+                      + (dimes * 0.10)
+                      + (nickels * 0.05)
+                      + (pennies * 0.01)
 
-    const expectedChange = {
-      quarters: noQuarters,
-      dimes:    noDimes,
-      nickels:  noNickels,
-      pennies:  noPennies
-    }
+    const expectedChange = { quarters, dimes, nickels, pennies }
 
-    expect(makeChange(price, amountGiven)).to.deep.equal(expectedChange)
+    expect(makeChange({price, amountGiven})).to.deep.equal(expectedChange)
   })
 })
--- src/makeChange.js
+++ src/makeChange.js
@@ -1,5 +1,5 @@
 'use strict'
 
-function makeChange(price, amountGiven) {
+export default function makeChange({price, amountGiven}) {
   // your code here
 }
