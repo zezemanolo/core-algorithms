8871f95cfe4bbd96d69abbff55f434ffd03c070f
lizzkats <katsnelson.lizz@gmail.com>
2017-01-24 14:46:53 -0800

    adds fizzbuzz implementation

--- test/fizzBuzz_test.js
+++ test/fizzBuzz_test.js
@@ -1,25 +1,26 @@
 import { expect } from 'chai'
-import fizzBuzz from '../src/fizzBuzz'
+import fizzBuzz from '../src/fizzBuzz.js'
 
 describe('fizzBuzz()', function(){
 
   it('should be a function', function(){
-    expect(fizzBuzz()).to.be.a('function')
+    expect(fizzBuzz).to.be.a('function')
   })
 
   it('should return fizz for multiples of three', function(){
-    expect(fizzBuzz()).to.equal(myArray[2] == 'Fizz')
+
+    expect(fizzBuzz()[2]).to.equal('Fizz')
   })
 
   it('should return buzz for multiples of five', function(){
-    expect(fizzBuzz()).to.equal(myArray[4] == 'Buzz')
+    expect(fizzBuzz()[4]).to.equal('Buzz')
   })
 
   it('should return fizzbuzz for multiples of three', function(){
-    expect(fizzBuzz()).to.equal(myArray[14] == 'FizzBuzz')
+    expect(fizzBuzz()[14]).to.equal('FizzBuzz')
   })
 
   it('should return an array with a length of 100', function(){
     expect(fizzBuzz()).to.have.length(100)
   })
-}
+})
