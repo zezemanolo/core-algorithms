be13b49308fc6e16e015e06fb6d57c3b0b7f478d
lizzkats <katsnelson.lizz@gmail.com>
2017-01-26 11:23:17 -0800

    adds setIntersection function and test

---
+++ test/setIntersection_test.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai'
+import setIntersection from '../src/setIntersection.js'
+
+describe('setIntersection()', function(){
+  it('should be a function', function(){
+    expect(setIntersection).to.be.a('function')
+  })
+
+  it('should return the union of the two sets', function(){
+    expect(setIntersection([1,2,3,4], [2,3,4,5])).to.eql([2,3,4])
+    expect(setIntersection([1,4,3], [2,6,4,5])).to.eql([4])
+  })
+})
